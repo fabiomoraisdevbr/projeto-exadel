f44c65bbdbf10492de819b13fb49e3627c905d1b
fabiomoraisdevbr <rarinaldo@outlook.com>
2026-05-03 17:03:30 -0300

    redis config (demorou uma eternidade pra funcionar)

---
+++ ms-customer/src/main/java/fabiomorais/dev/br/ms_customer/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package fabiomorais.dev.br.ms_customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(
+            ReactiveRedisConnectionFactory factory) {
+
+        RedisSerializationContext<String, Object> context =
+                RedisSerializationContext
+                        .<String, Object>newSerializationContext(RedisSerializer.string())
+                        .value(RedisSerializer.json())
+                        .build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+}
