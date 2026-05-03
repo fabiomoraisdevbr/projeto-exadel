dd552c9c0703934fef28b8f45456b2531b91b2f2
fabiomoraisdevbr <rarinaldo@outlook.com>
2026-05-03 17:04:14 -0300

    ajuste de configs

--- ms-customer/src/main/java/fabiomorais/dev/br/ms_customer/entity/CustomerEntity.java
+++ ms-customer/src/main/java/fabiomorais/dev/br/ms_customer/entity/CustomerEntity.java
@@ -1,14 +1,16 @@
-package fabiomorais.dev.br.ms_customer;
+package fabiomorais.dev.br.ms_customer.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Data
 @Table("customer")
 public class CustomerEntity {
 
         @Id
         private Integer id;
 
-        private String name;
+        private String customer_name;
         private String email;
 }
--- ms-customer/src/main/java/fabiomorais/dev/br/ms_customer/repository/CustomerRepository.java
+++ ms-customer/src/main/java/fabiomorais/dev/br/ms_customer/repository/CustomerRepository.java
@@ -1,7 +1,8 @@
-package fabiomorais.dev.br.ms_customer;
+package fabiomorais.dev.br.ms_customer.repository;
 
-import fabiomorais.dev.br.ms_customer.CustomerEntity;
+import fabiomorais.dev.br.ms_customer.entity.CustomerEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface CustomerRepository extends ReactiveCrudRepository<CustomerEntity, Long> {
 }
