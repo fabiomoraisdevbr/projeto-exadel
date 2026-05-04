9467f1b2b74c46326eb7d98c9523b9ddac07bf2c
fabiomoraisdevbr <rarinaldo@outlook.com>
2026-05-04 04:43:34 -0300

    config do rabbitmq

---
+++ ms-customer/src/main/java/fabiomorais/dev/br/ms_customer/config/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package fabiomorais.dev.br.ms_customer.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_REQUEST= "order.total.request";
+    public static final String QUEUE_RESPONSE= "order.total.response";
+
+    @Bean
+    public Queue orderTotalRequestQueue() {
+        return new Queue(QUEUE_REQUEST, true);
+    }
+
+    @Bean
+    public Queue orderTotalResponseQueue() {
+        return new Queue(QUEUE_RESPONSE, true);
+    }
+}
